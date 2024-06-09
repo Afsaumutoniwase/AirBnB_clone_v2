bf21739b9de6d703faeb4628e5879c74172e6833
Afsa Umutoniwase <a.umutoniwa@alustudent.com>
2024-06-09 13:25:43 +0200

    Update 1-pack_web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,24 +6,24 @@ the function do_pack."""
 
 from datetime import datetime
 import os
-
-# 1-pack_web_static.py
 from fabric import task
-from invoke import run
 
 @task
 def do_pack(c):
-    run("tar -cvzf web_static.tgz web_static")
-
-def do_pack():
     """Function to generate a .tgz archive from the contents of the web_static
     folder."""
 
     time_stamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    local("mkdir -p versions")
     archive_path = "versions/web_static_{}.tgz".format(time_stamp)
-    local("tar -cvzf {} web_static".format(archive_path))
-    if os.path.exists(archive_path):
+    
+    # Create the versions directory if it doesn't exist
+    os.makedirs("versions", exist_ok=True)
+    
+    # Run the tar command to create the archive
+    result = c.run("tar -cvzf {} web_static".format(archive_path), warn=True)
+    
+    # Check if the archive was created successfully
+    if result.ok and os.path.exists(archive_path):
         return archive_path
     else:
         return None
